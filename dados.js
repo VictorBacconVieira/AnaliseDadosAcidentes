a62a14119b51838af6b19a336ae412d4efdadf13
Victor Baccon Vieira <victorbacconvieira@gmail.com>
2026-03-25 23:34:40 +0000

    Add: Adiciona dados e código

---
+++ dados.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+
+// Lê o arquivo CSV
+const texto = fs.readFileSync("e.csv", "utf-8");
+
+// Quebra em linhas
+const linhas = texto.split("\n");
+
+let x = [];
+let y = [];
+
+
+// Ignora o cabeçalho (linha 0)
+for (let i = 1; i < linhas.length; i++) {
+    let valores = linhas[i].split(",");
+
+    if (valores.length >= 2) {
+        x.push(Number(valores[0]));
+        y.push(Number(valores[1]));
+    }
+}
+
+// Função de regressão
+function calcularRegressao(x, y) {
+    let n = x.length;
+
+    let somaX = 0, somaY = 0, somaXY = 0, somaX2 = 0;
+
+    for (let i = 0; i < n; i++) {
+        somaX += x[i];
+        somaY += y[i];
+        somaXY += x[i] * y[i];
+        somaX2 += x[i] * x[i];
+    }
+
+    let b = (n * somaXY - somaX * somaY) / (n * somaX2 - somaX * somaX);
+    let a = (somaY - b * somaX) / n;
+
+    let mediaY = somaY / n;
+    let ssTotal = 0;
+    let ssRes = 0;
+
+    for (let i = 0; i < n; i++) {
+        let yPrev = a + b * x[i];
+        ssTotal += Math.pow(y[i] - mediaY, 2);
+        ssRes += Math.pow(y[i] - yPrev, 2);
+    }
+
+    let r2 = 1 - (ssRes / ssTotal);
+
+    console.log("Equação: y = " + a.toFixed(2) + " + " + b.toFixed(2) + "x");
+    console.log("R² = " + r2.toFixed(4));
+}
+
+// Chamada
+calcularRegressao(x, y);
